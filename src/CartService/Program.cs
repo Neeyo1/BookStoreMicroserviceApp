a66d3192a7bfb7610af95738f724ebe219d57db5
Neeyo <jakub.nycz99@gmail.com>
2025-02-04 14:14:48 +0100

    Add configuration to cart service

--- src/CartService/Program.cs
+++ src/CartService/Program.cs
@@ -1,8 +1,32 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddMassTransit(x => 
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("carts", false));
+    x.UsingRabbitMq((context, conf) =>
+    {
+        conf.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+        {
+            options.Authority = builder.Configuration["IdentityServiceUrl"];
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters.ValidateAudience = false;
+            options.TokenValidationParameters.NameClaimType = "username";
+        }
+    );
 
 var app = builder.Build();
 
@@ -11,5 +35,8 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("CartDB", 
+    MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
 
 app.Run();
