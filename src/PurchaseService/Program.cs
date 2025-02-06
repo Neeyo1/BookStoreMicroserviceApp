1890627cfc8953115ef9a20f93f0f327323d131f
Neeyo <jakub.nycz99@gmail.com>
2025-02-06 13:32:29 +0100

    Add configuration to purchase service

--- src/PurchaseService/Program.cs
+++ src/PurchaseService/Program.cs
@@ -1,15 +1,54 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddMassTransit(x => 
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("purchace", false));
+
+    x.UsingRabbitMq((context, conf) =>
+    {
+        conf.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+        {
+            options.Authority = builder.Configuration["IdentityServiceUrl"];
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters.ValidateAudience = false;
+            options.TokenValidationParameters.NameClaimType = "username";
+        }
+    );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var connectionString = app.Configuration.GetConnectionString("MongoDbConnection");
+    await DB.InitAsync("PurchaseDb", MongoClientSettings.FromConnectionString(connectionString));
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
